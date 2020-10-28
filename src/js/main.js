7a86236e0e4f5b6f6449b15d5ce0f3a0df083466
Chermaley <slimix23@gmail.com>
2020-10-28 09:14:07 +0300

    add modal script

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,33 @@
 import $ from './lib/lib';
+$('#trigger').click(() => $('#trigger').createModal({
+    text: {
+        title: 'Modal title w',
+        body: 'lorem takoi lorem'
+    },
+    btns: {
+        count: 3,
+        settings: [
+            [
+                'Close',
+                ['btn-dander', 'mr-10'],
+                true
+            ],
+            [
+                'Save changes',
+                ['btn-success'],
+                false,
+                () => {
+                    alert('Данные сохранены');
+                }
+            ],
+            [
+                'sasa',
+                ['btn-warning', 'ml-10'],
+                false,
+                () => {
+                    alert('пиздец');
+                }
+            ]
+        ]
+    }
+}));
